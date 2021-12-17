0d8126d254d4aa2992070f48b31e0d7206234b31
Ryan Zeigler <zeiglerr@gmail.com>
2021-12-16 19:11:01 -0600

    Refactor

--- io.h
+++ io.h
@@ -2,6 +2,9 @@
 #define UTIL_H
 
 #include <stdio.h>
+
+#include "text.h"
+
 typedef struct buffered_reader {
     FILE *file;
     char *buffer;
@@ -20,36 +23,5 @@ void buffered_reader_release(buffered_reader *in_buf);
 void buffered_reader_uninit(buffered_reader *in_buf);
 
 char *buffered_reader_read(buffered_reader *buf);
-
-typedef struct tokenizer {
-    char *source;
-    char *start;
-    char *end;
-    char *split_by;
-} tokenizer;
-
-// assumes ownership of source (needs free), does not take ownership of split_by
-tokenizer *tokenizer_create(char *source, char *split_by);
-
-int tokenizer_init(tokenizer *tokz, char *source, char *split_by);
-
-void tokenizer_release(tokenizer *tokz);
-
-void tokenizer_uninit(tokenizer *tokz);
-
-// Pointer to the next occurrence in tokz along with length
-char *tokenizer_next(tokenizer *tokz, size_t *len);
-
-typedef struct str {
-    char *str;
-    size_t str_len;
-} str;
-
-unsigned int str_atoui(str *s);
-
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-
-#define CHAR_DIGIT(c) (((unsigned int)c) - 48)
 
 #endif
---
+++ text.h
@@ -0,0 +1,39 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <stdlib.h>
+
+char *common_needle_search(char *initial_pos, char *end_pos, char *needle);
+
+typedef struct tokenizer {
+    char *source;
+    char *start;
+    char *end;
+    char *split_by;
+} tokenizer;
+
+// assumes ownership of source (needs free), does not take ownership of split_by
+tokenizer *tokenizer_create(char *source, char *split_by);
+
+int tokenizer_init(tokenizer *tokz, char *source, char *split_by);
+
+void tokenizer_release(tokenizer *tokz);
+
+void tokenizer_uninit(tokenizer *tokz);
+
+// Pointer to the next occurrence in tokz along with length
+char *tokenizer_next(tokenizer *tokz, size_t *len);
+
+typedef struct str {
+    char *str;
+    size_t str_len;
+} str;
+
+unsigned int str_atoui(str *s);
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#define CHAR_DIGIT(c) (((unsigned int)c) - 48)
+
+#endif
